4850c774abe3e32e728e3982dee1d46c883f8397
Akshay Katariya <Akshay.katariya1@gmail.com>
2023-11-28 20:04:02 +0530

    password hashing using bcrypt and middleware

--- Models/Users.model.js
+++ Models/Users.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   email: {
@@ -17,6 +18,18 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+// Called before saving a user
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model("user", userSchema);
