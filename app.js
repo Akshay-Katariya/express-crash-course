1f6fc57f16279571974a333dbdbe61069bb549fb
Akshay Katariya <Akshay.katariya1@gmail.com>
2023-11-28 18:19:08 +0530

    setting up express with error handler

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello 👋");
+});
+
+app.use((req, res, next) => {
+  // method 1:
+  // const error = new Error("Not Found");
+  // error.status = 404;
+  // next(error);
+  // ---------------
+  // Method 2: createError
+  next(createError.NotFound("This rout does not exist"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
